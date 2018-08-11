7df153f4a45342931b90e2a0703d33ac4a5cb3ce
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-08-11 09:20:04 +0200

    19.1 Updated 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://elegant-gerard-40334.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://elegant-gerard-40334.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
